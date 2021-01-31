0440bbc431e8161c599bacb81a58d94d0d445daf
Konrad Weihmann <kweihmann@outlook.com>
2021-01-31 16:01:36 +0100

    Test fixes
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- lib/oeqa/runtime/cases/rubygems_utils.py
+++ lib/oeqa/runtime/cases/rubygems_utils.py
@@ -1,16 +1,18 @@
 from oeqa.runtime.case import OERuntimeTestCase
 import sys
 
+
 class RubyGemsTestUtils(OERuntimeTestCase):
 
     def gem_is_installed(self, gemname):
-        status, output = self.target.run("GEM_PATH=/usr/lib/ruby/site_ruby:/usr/lib/ruby/gems gem list")
+        status, output = self.target.run("gem list")
         sys.stderr.write(output + "\n")
         output = output.split("\n")
-        self.assertTrue(any(x.startswith(gemname + " ") for x in output), msg="%s should be installed" % gemname)
+        self.assertTrue(any(x.startswith(gemname + " ") for x in output), 
+                        msg="%s should be installed. Installed %s" % (gemname, ",".join(output)))
 
     def gem_is_loadable(self, require):
-        status, output = self.target.run("GEM_PATH=/usr/lib/ruby/site_ruby:/usr/lib/ruby/gems ruby -e 'require %s'" % require)
-        sys.stderr.write(output + "\n")
-        self.assertEquals(status, 0, msg="%s should be loadable" % require)
-        
+        _, _=self.target.run(
+            "echo \"require '%s'\" > /tmp/_rubygems.test" % require)
+        status, output=self.target.run("ruby /tmp/_rubygems.test")
+        self.assertEquals(status, 0, msg="%s should be loadable. ruby output: %s" % (require, output))
