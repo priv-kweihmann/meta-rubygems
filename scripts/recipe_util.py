6e86ad257011f63e235e59b24e4caa3faa2a1702
Konrad Weihmann <kweihmann@outlook.com>
2021-02-01 19:22:54 +0100

    recipe-utils fixes
    - use tempdir from python
    - save a few vars from old recipes
    - some format fixes
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- scripts/recipe_util.py
+++ scripts/recipe_util.py
@@ -12,34 +12,33 @@ try:
 except ImportError:
     sys.stderr.write("Can't import 'oelint-parser'. Please run 'pip install oelint-parser' to enable this script here\n")
 
-def __get_info_from_stash(_filepath):
-    _rdepends = set()
-    _depends = set()
+def __get_info_from_stash(_filepath, name, modifiers):
+    _result = set()
 
     _stash = Stash(quiet=True)
     _stash.AddFile(_filepath)
            
-    for item in _stash.GetItemsFor(attribute=Variable.ATTR_VAR, attributeValue="RDEPENDS_${PN}"):
-        _rdepends.update([expand_term(_stash, _filepath, y)
+    for item in _stash.GetItemsFor(attribute=Variable.ATTR_VAR, attributeValue=name):
+        if modifiers and not all(x in modifiers for x in item.SubItems):
+            continue
+        _result.update([expand_term(_stash, _filepath, y)
                             for y in item.get_items()])
 
-    # Get build time dependencies
-    for item in _stash.GetItemsFor(attribute=Variable.ATTR_VAR, attributeValue="DEPENDS"):
-        _depends.update([expand_term(_stash, _filepath, y)
-                            for y in item.get_items()])
-
-    return (_depends, _rdepends)
+    return _result
 
 def check_depends(depends, rdepends, oldrecipes):
     _rdepends = set()
     _depends = set()
     for x in oldrecipes:
-        d, r = __get_info_from_stash(x)
-        _depends.update(d)
-        _rdepends.update(r)
+        _depends.update(__get_info_from_stash(x, "DEPENDS", ["class-native"]))
+        _rdepends.update(__get_info_from_stash(x, "RDEPENDS_${PN}", ["class-target"]))
     
     depends = set(list(depends) + list(_depends))
     rdepends = set(list(rdepends) + list(_rdepends))
     return (depends, rdepends)
 
-    
+def save_variables(name, modifiers, oldrecipes):
+    old_values = set()
+    for x in oldrecipes:
+        old_values.update(__get_info_from_stash(x, name, modifiers))
+    return old_values
