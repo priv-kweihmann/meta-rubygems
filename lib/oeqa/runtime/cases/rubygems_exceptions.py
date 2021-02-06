337900f855e66533a2f08cace1c4116a44ab0141
Konrad Weihmann <kweihmann@outlook.com>
2021-02-06 12:06:13 +0100

    Put test exceptions into seperate class
    + add possibility to skip tests
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ lib/oeqa/runtime/cases/rubygems_exceptions.py
@@ -0,0 +1,19 @@
+class RubyGemsTestExceptions():
+
+    exec_wrapper_return_codes = {
+        "htmldiff": 127,
+        "rwinrm": 1,
+        "rwinrmcp": 1
+    }
+
+    exec_wrapper_skips = {
+        "setup": "unknown issue with ruby load function"
+    }
+
+    loadable_skips = {
+
+    }
+
+    gem_list_skips = {
+
+    }
--- lib/oeqa/runtime/cases/rubygems_utils.py
+++ lib/oeqa/runtime/cases/rubygems_utils.py
@@ -1,16 +1,14 @@
 from oeqa.runtime.case import OERuntimeTestCase
+from rubygems_exceptions import RubyGemsTestExceptions
 import sys
 
 
 class RubyGemsTestUtils(OERuntimeTestCase):
 
-    EXEC_EXCEPTIONS = {
-        "htmldiff": 127,
-        "rwinrm": 1,
-        "rwinrmcp": 1
-    }
-
     def gem_is_installed(self, gemname):
+        _skip = RubyGemsTestExceptions.gem_list_skips(gemname, '')
+        if _skip:
+            self.skipTest("'%s' due to '%s'" % (gemname, _skip))
         status, output = self.target.run("gem list")
         sys.stderr.write(output + "\n")
         output = output.split("\n")
@@ -18,11 +16,17 @@ class RubyGemsTestUtils(OERuntimeTestCase):
                         msg="%s should be installed. Installed %s" % (gemname, ",".join(output)))
 
     def gem_is_loadable(self, require):
+        _skip = RubyGemsTestExceptions.loadable_skips(require, '')
+        if _skip:
+            self.skipTest("'%s' due to '%s'" % (require, _skip))
         _, _ = self.target.run("echo \"require '%s'\" > /tmp/_rubygems.test" % require)
         status, output = self.target.run("ruby /tmp/_rubygems.test")
         self.assertEquals(status, 0, msg="%s should be loadable. ruby output: %s" % (require, output))
 
     def gem_exec_wrapper(self, _exec):
-        _expret = RubyGemsTestUtils.EXEC_EXCEPTIONS.get(_exec, 0)
+        _skip = RubyGemsTestExceptions.exec_wrapper_skips(_exec, '')
+        if _skip:
+            self.skipTest("'%s' due to '%s'" % (_exec, _skip))
+        _expret = RubyGemsTestExceptions.exec_wrapper_return_codes.get(_exec, 0)
         status, output = self.target.run("%s --help" % _exec)
         self.assertEquals(status, _expret, msg="%s exec is runnable. output: %s" % (_exec, output))
