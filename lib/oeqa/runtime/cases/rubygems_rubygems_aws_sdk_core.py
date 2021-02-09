1c93909e0a1905d98a67269873a3dc486b44b80b
Konrad Weihmann <kweihmann@outlook.com>
2021-02-09 12:20:34 +0100

    gems: add aws-sdk-core
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ lib/oeqa/runtime/cases/rubygems_rubygems_aws_sdk_core.py
@@ -0,0 +1,19 @@
+from rubygems_utils import RubyGemsTestUtils
+
+class RubyGemsTestrubygems_aws_sdk_core(RubyGemsTestUtils):
+
+    def test_gem_list_rubygems_aws_sdk_core(self):
+        self.gem_is_installed("aws-sdk-core")
+
+    def test_load_aws_sdk_core(self):
+        self.gem_is_loadable("aws-sdk-core")
+
+    def test_load_aws_sdk_sso(self):
+        self.gem_is_loadable("aws-sdk-sso")
+
+    def test_load_aws_sdk_sts(self):
+        self.gem_is_loadable("aws-sdk-sts")
+
+    def test_load_seahorse(self):
+        self.gem_is_loadable("seahorse")
+
