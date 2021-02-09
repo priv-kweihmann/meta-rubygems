80cb7b2ee62795854a77063e8854d78963db0bf4
Konrad Weihmann <kweihmann@outlook.com>
2021-02-09 09:43:14 +0100

    lint: add layer specific linting rules
    this layer uses oelint-adv to prevent several bad pattern
    to be merged.
    this commit adds custom rules and the layer specific
    confiiguration
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

---
+++ files/lint/oelint-custom/rule_SPDX.py
@@ -0,0 +1,19 @@
+from oelint_adv.cls_rule import Rule
+from oelint_parser.cls_item import Comment
+
+
+class RubygemsSPDX(Rule):
+
+    def __init__(self):
+        super().__init__(id="rubygems.spdxheader",
+                         severity="error",
+                         message="Please add an 'SPDX-License-Identifier: MIT' comment to the file")
+
+    def check(self, _file, stash):
+        res = []
+        items = stash.GetItemsFor(
+            filename=_file, classifier=Comment.CLASSIFIER)
+        found = any(x for x in items if "SPDX-License-Identifier: MIT" in x.Raw)
+        if not found:
+            res += self.finding(_file, 1)
+        return res
---
+++ files/lint/oelint-custom/rule_testcase.py
@@ -0,0 +1,40 @@
+import os
+
+from oelint_adv.cls_rule import Rule
+from oelint_parser.helper_files import expand_term
+from oelint_parser.helper_files import get_layer_root
+
+
+class RubygemsTestCase(Rule):
+
+    TESTCASE_DIR = "lib/oeqa/runtime/cases"
+
+    def __init__(self):
+        super().__init__(id="rubygems.testcase",
+                         severity="error",
+                         message="Recipe has to have a test case")
+
+    def __sanitize_pn(self, name):
+        return name.replace("@", "").replace("/", "-").replace("-", "_")
+
+    def __needle_to_search_for(self, name):
+        return "class RubyGemsTest{pn}(RubyGemsTestUtils)".format(pn=self.__sanitize_pn(name))
+
+    def check(self, _file, stash):
+        res = []
+
+        found = False
+        _pn = expand_term(stash, _file, "${PN}")
+        _layer_root = get_layer_root(_file)
+        _needle = self.__needle_to_search_for(_pn)
+        for root, dirs, files in os.walk(os.path.join(_layer_root, RubygemsTestCase.TESTCASE_DIR)):
+            for f in files:
+                if not f.endswith(".py"):
+                    continue
+                with open(os.path.join(root, f)) as i:
+                    if _needle in i.read():
+                        found = True
+                        break
+        if not found:
+            res += self.finding(_file, 1)
+        return res
