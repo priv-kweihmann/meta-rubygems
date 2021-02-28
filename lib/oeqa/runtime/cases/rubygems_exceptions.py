5f171dc1196b57c803832478bcb90fcb7c99c385
Konrad Weihmann <kweihmann@outlook.com>
2021-02-28 16:46:47 +0100

    tests: add exceptions for new gems
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- lib/oeqa/runtime/cases/rubygems_exceptions.py
+++ lib/oeqa/runtime/cases/rubygems_exceptions.py
@@ -7,11 +7,25 @@ class RubyGemsTestExceptions():
     }
 
     exec_wrapper_skips = {
+        "gauntlet": "some internal weirdness",
         "setup": "unknown issue with ruby load function"
     }
 
     loadable_skips = {
-        "syslog-formatter": "module needs upfront configuration of a logging provider"
+        "gauntlet_flay": "causes a SIGKILL",
+        "gauntlet_flog": "causes a SIGKILL",
+        "gauntlet_grep": "causes a SIGKILL",
+        "gauntlet_parser": "causes a SIGKILL",
+        "ruby20_parser": "not the parser you are looking for",
+        "ruby21_parser": "not the parser you are looking for",
+        "ruby22_parser": "not the parser you are looking for",
+        "ruby23_parser": "not the parser you are looking for",
+        "ruby24_parser": "not the parser you are looking for",
+        "ruby25_parser": "not the parser you are looking for",
+        "ruby26_parser": "not the parser you are looking for",
+        "ruby27_parser": "not the parser you are looking for",
+        "simplecov-html": "needs extra configuration -> uninitialized constant SimpleCov (NameError)",
+        "syslog-formatter": "module needs upfront configuration of a logging provider",
     }
 
     gem_list_skips = {
