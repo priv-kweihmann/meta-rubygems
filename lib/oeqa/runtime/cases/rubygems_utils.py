2e5695e5cf51156c9f14ea045ddf3aeb04913ccc
Konrad Weihmann <kweihmann@outlook.com>
2021-03-01 20:58:05 +0100

    tests: cache ruby gem list
    as the gem list is static after booting, it doesn't make sense run
    "gem list" each and every time for each gem sepately.
    Run "gem list" once and reuse the result for all test cases requesting
    that info. This heavily speeds up the overall execution speed
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- lib/oeqa/runtime/cases/rubygems_utils.py
+++ lib/oeqa/runtime/cases/rubygems_utils.py
@@ -1,32 +1,44 @@
 from oeqa.runtime.case import OERuntimeTestCase
 from rubygems_exceptions import RubyGemsTestExceptions
-import sys
+
+gem_list = set()
 
 
 class RubyGemsTestUtils(OERuntimeTestCase):
+
+    def __gem_list(self):
+        global gem_list
+        if not gem_list:
+            status, output = self.target.run("gem list")
+            for line in output.split("\n"):
+                if not line.strip():
+                    continue
+                gem_list.add(line.split(" ")[0])
+        return gem_list
 
     def gem_is_installed(self, gemname):
         _skip = RubyGemsTestExceptions.gem_list_skips.get(gemname, '')
         if _skip:
             self.skipTest("'%s' skipped due to '%s'" % (gemname, _skip))
-        status, output = self.target.run("gem list")
-        sys.stderr.write(output + "\n")
-        output = output.split("\n")
-        self.assertTrue(any(x.startswith(gemname + " ") for x in output),
-                        msg="%s should be installed. Installed %s" % (gemname, ",".join(output)))
+        self.assertTrue(gemname in self.__gem_list(),
+                        msg="%s should be installed. Installed %s" % (gemname, ",".join(self.__gem_list())))
 
     def gem_is_loadable(self, require):
         _skip = RubyGemsTestExceptions.loadable_skips.get(require, '')
         if _skip:
             self.skipTest("'%s' skipped due to '%s'" % (require, _skip))
-        _, _ = self.target.run("echo \"require '%s'\" > /tmp/_rubygems.test" % require)
+        _, _ = self.target.run(
+            "echo \"require '%s'\" > /tmp/_rubygems.test" % require)
         status, output = self.target.run("ruby /tmp/_rubygems.test")
-        self.assertEquals(status, 0, msg="%s should be loadable. ruby output: %s" % (require, output))
+        self.assertEquals(
+            status, 0, msg="%s should be loadable. ruby output: %s" % (require, output))
 
     def gem_exec_wrapper(self, _exec):
         _skip = RubyGemsTestExceptions.exec_wrapper_skips.get(_exec, '')
         if _skip:
             self.skipTest("'%s' skipped due to '%s'" % (_exec, _skip))
-        _expret = RubyGemsTestExceptions.exec_wrapper_return_codes.get(_exec, 0)
+        _expret = RubyGemsTestExceptions.exec_wrapper_return_codes.get(
+            _exec, 0)
         status, output = self.target.run("%s --help" % _exec)
-        self.assertEquals(status, _expret, msg="%s exec is runnable. output: %s" % (_exec, output))
+        self.assertEquals(
+            status, _expret, msg="%s exec is runnable. output: %s" % (_exec, output))
