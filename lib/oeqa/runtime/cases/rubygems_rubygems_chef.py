4a3e6dce0461db0294ed90513b31696e72ce755d
Konrad Weihmann <kweihmann@outlook.com>
2021-05-02 10:51:26 +0200

    Update rubygems-chef to 17.0.242
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- lib/oeqa/runtime/cases/rubygems_rubygems_chef.py
+++ lib/oeqa/runtime/cases/rubygems_rubygems_chef.py
@@ -2,9 +2,6 @@ from rubygems_utils import RubyGemsTestUtils
 
 class RubyGemsTestrubygems_chef(RubyGemsTestUtils):
 
-    def test_exec_knife(self):
-        self.gem_exec_wrapper("knife")
-
     def test_gem_list_rubygems_chef(self):
         self.gem_is_installed("chef")
 
