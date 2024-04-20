501ce64b5fc5847fbe75b00427010a429180eead
Konrad Weihmann <kweihmann@outlook.com>
2024-04-20 02:41:41 +0000

    ruby_parser: update to 3.21.0

--- lib/oeqa/runtime/cases/rubygems_rubygems_ruby_parser.py
+++ lib/oeqa/runtime/cases/rubygems_rubygems_ruby_parser.py
@@ -17,39 +17,6 @@ class RubyGemsTestrubygems_ruby_parser(RubyGemsTestUtils):
     def test_load_rp_stringscanner(self):
         self.gem_is_loadable("rp_stringscanner")
 
-    def test_load_ruby20_parser(self):
-        self.gem_is_loadable("ruby20_parser")
-
-    def test_load_ruby21_parser(self):
-        self.gem_is_loadable("ruby21_parser")
-
-    def test_load_ruby22_parser(self):
-        self.gem_is_loadable("ruby22_parser")
-
-    def test_load_ruby23_parser(self):
-        self.gem_is_loadable("ruby23_parser")
-
-    def test_load_ruby24_parser(self):
-        self.gem_is_loadable("ruby24_parser")
-
-    def test_load_ruby25_parser(self):
-        self.gem_is_loadable("ruby25_parser")
-
-    def test_load_ruby26_parser(self):
-        self.gem_is_loadable("ruby26_parser")
-
-    def test_load_ruby27_parser(self):
-        self.gem_is_loadable("ruby27_parser")
-
-    def test_load_ruby30_parser(self):
-        self.gem_is_loadable("ruby30_parser")
-
-    def test_load_ruby31_parser(self):
-        self.gem_is_loadable("ruby31_parser")
-
-    def test_load_ruby32_parser(self):
-        self.gem_is_loadable("ruby32_parser")
-
     def test_load_ruby_lexer(self):
         self.gem_is_loadable("ruby_lexer")
 
@@ -61,6 +28,42 @@ class RubyGemsTestrubygems_ruby_parser(RubyGemsTestUtils):
 
     def test_load_ruby_parser(self):
         self.gem_is_loadable("ruby_parser")
+
+    def test_load_ruby_parser20(self):
+        self.gem_is_loadable("ruby_parser20")
+
+    def test_load_ruby_parser21(self):
+        self.gem_is_loadable("ruby_parser21")
+
+    def test_load_ruby_parser22(self):
+        self.gem_is_loadable("ruby_parser22")
+
+    def test_load_ruby_parser23(self):
+        self.gem_is_loadable("ruby_parser23")
+
+    def test_load_ruby_parser24(self):
+        self.gem_is_loadable("ruby_parser24")
+
+    def test_load_ruby_parser25(self):
+        self.gem_is_loadable("ruby_parser25")
+
+    def test_load_ruby_parser26(self):
+        self.gem_is_loadable("ruby_parser26")
+
+    def test_load_ruby_parser27(self):
+        self.gem_is_loadable("ruby_parser27")
+
+    def test_load_ruby_parser30(self):
+        self.gem_is_loadable("ruby_parser30")
+
+    def test_load_ruby_parser31(self):
+        self.gem_is_loadable("ruby_parser31")
+
+    def test_load_ruby_parser32(self):
+        self.gem_is_loadable("ruby_parser32")
+
+    def test_load_ruby_parser33(self):
+        self.gem_is_loadable("ruby_parser33")
 
     def test_load_ruby_parser_extras(self):
         self.gem_is_loadable("ruby_parser_extras")
